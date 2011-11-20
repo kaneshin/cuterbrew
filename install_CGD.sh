285204d243c3ddb1740eb639ec932a17e13691e1
Shintaro Kaneko <kaneshin0120@gmail.com>
2011-11-20 11:23:03 +0900

    Add install_CGD.sh

---
+++ install_CGD.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#===========================================================================
+# File: install_CGD.sh
+# Version: 0.0.1
+# Last Change: 11-Nov-2011.
+# Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
+# Description:
+#   TODO
+#===========================================================================
+
+# set variables
+RUNDIR=`dirname ${0}`
+OPTIMHOME=~/optim
+CGDDIR=CG_DESCENT
+CGDHOME=$OPTIMHOME/$CGDDIR
+CUTEINTF=$CGDHOME/CUTEr_interface
+
+cd $RUNDIR
+cp run_cg_descent.sh $OPTIMHOME
+
+# wget
+cd $OPTIMHOME/src
+wget http://www.math.ufl.edu/~hager/papers/CG/Archive/CG_DESCENT-C-5.0.tar.gz
+
+# install CG_DESCENT
+cd $OPTIMHOME
+tar zxvf src/CG_DESCENT-C-5.0.tar.gz 2>&1 > $CGDDIR.log
+mv CG_DESCENT-C-5.0/ $CGDDIR/
+mv CG_DESCENT.log $CGDDIR/
+
+cd $CUTEINTF
+cp * $CGDHOME
+
+cd $OPTIMHOME
+mv run_cg_descent.sh $CGDHOME
+
+# EOF
---
+++ run_cg_descent.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 1. Put cg_user.h into $CUTER/common/include
+CGD=~/optim/CGD_5.0
+cd $CGD
+cp cg_user.h $CUTER/common/include
+# 2. Put cg_descentma.c into $CUTER/common/src/tools
+cp cg_descentma.c  $CUTER/common/src/tools
+# 3. In $CUTER/common/src/tools, "gcc -lm -O3 -c cg_descentma.c"
+cd $CUTER/common/src/tools
+gcc -lm -O3 -c cg_descentma.c
+# 4. "cp cg_descentma.o $MYCUTER/double/bin"
+cp cg_descentma.o $MYCUTER/double/bin
+# 5. In the directory where you put cg_descent, type "make" and then 
+#    "cp cg_descent.o $MYCUTER/double/bin"
+cd $CGD
+make
+cp cg_descent.o $MYCUTER/double/bin
+# 6. "cp cg_descent.pro $CUTER/build/prototypes"
+#    "cp sdcg_descent.pro $CUTER/build/prototypes"
+cp cg_descent.pro $CUTER/build/prototypes
+cp sdcg_descent.pro $CUTER/build/prototypes
+# 7. "cd $MYCUTER/bin"
+cd $MYCUTER/bin
+# 8. type the following command twice:
+#    where "pack" is first "cg_descent" and then "sdcg_descent"
+sed -f $MYCUTER/double/config/script.sed $CUTER/build/prototypes/cg_descent.pro > cg_descent
+sed -f $MYCUTER/double/config/script.sed $CUTER/build/prototypes/sdcg_descent.pro > sdcg_descent
+# 9. "chmod a+x cg_descent" and "chmod a+x sdcg_descent"
+chmod a+x cg_descent
+chmod a+x sdcg_descent
+
+# ext. delete object files
+# rm $CUTER/common/src/tools/cg_descentma.o
+# rm $CGD/cg_descent.o
