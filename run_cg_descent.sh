39467ca5105c88d7b94b86fe58f105577a3c8272
Shintaro Kaneko <kaneshin0120@gmail.com>
2011-12-06 18:04:57 +0900

    Mod OPTIM=>WORK on run

--- run_cg_descent.sh
+++ run_cg_descent.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ "$OPTIMHOME" == "" ]]; then
+if [[ "$WORKHOME" == "" ]]; then
     echo "You must do a \"source .cuterrc\"."
     exit 1
 fi
 
 # 1. Put cg_user.h into $CUTER/common/include
-CGD=$OPTIMHOME/CG_DESCENT
+CGD=$WORKHOME/CG_DESCENT
 cd $CGD
 cp cg_user.h $CUTER/common/include
 # 2. Put cg_descentma.c into $CUTER/common/src/tools
